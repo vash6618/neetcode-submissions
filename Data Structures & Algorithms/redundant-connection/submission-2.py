4b76450c25d8d7dd37cb430b01ed886fc05d5b1d
Vasu Sharma <78123739+vash6618@users.noreply.github.com>
2026-08-26 16:41:26 -0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    def dfs(self, adj_list, vis, node, parent, cycle_nodes):
+        vis[node] = True
+        cycle_nodes.append(node)
+        for neigh in adj_list[node]:
+            if not vis[neigh]:
+                cycle = self.dfs(adj_list, vis, neigh, node, cycle_nodes)
+                if cycle:
+                    return cycle
+            else:
+                if neigh != parent: # cycle
+                    return list(cycle_nodes[cycle_nodes.index(neigh):])
+        cycle_nodes.pop()
+        return None
+        
+
+
+
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        adj_list = [set() for i in range(len(edges) + 1)]
+        len
+        for edge in edges:
+            adj_list[edge[0]].add(edge[1])
+            adj_list[edge[1]].add(edge[0])
+        st = 0
+        result = []
+        vis = [False] * (len(edges) + 1)
+        vis[0] = True
+        running_cycle = []
+        final_cycle = self.dfs(adj_list, vis, 1, None, running_cycle)
+        final_cycle = set(final_cycle)
+        for edge in edges:
+            if edge[0] in final_cycle and edge[1] in final_cycle:
+                result = edge
+        return result
