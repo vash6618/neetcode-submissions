0cda22af96a5256f726df2c85d78f2d14a6f2f36
Vasu Sharma <78123739+vash6618@users.noreply.github.com>
2026-09-09 01:30:13 -0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,19 @@
+from itertools import islice
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        m = defaultdict(int)
+        for key in nums:
+            m[key] += 1
+        buckets = defaultdict(list)
+        ans, largest_key = [], 0
+        for key, cnt in m.items():
+            buckets[cnt].append(key)
+            largest_key = max(largest_key, cnt)
+        for i in range(largest_key, -1, -1):
+            if k <= 0:
+                break
+            sliced_set = buckets[i][:k]
+            ans += sliced_set
+            k -= len(sliced_set)
+        return ans
