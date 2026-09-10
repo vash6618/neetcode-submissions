631572677b5adb112fc2e5b027217a6cc88b21b1
Vasu Sharma <78123739+vash6618@users.noreply.github.com>
2026-09-09 17:51:31 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,25 @@
+import json
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for s in strs:
+            encoded.append(str(len(s)) + "#" + s)
+        return "".join(encoded)
+            
+
+    def decode(self, s: str) -> List[str]:
+        ind = 0
+        ans = []
+        curr_num = 0
+        while ind < len(s):
+            if s[ind] == "#":
+                ind += 1
+                ans.append(s[ind: ind + curr_num])
+                ind += curr_num
+                curr_num = 0
+            elif s[ind].isdigit():
+                curr_num = curr_num * 10 + int(s[ind])
+                ind += 1
+            else:
+                raise ValueError("malformed input at {ind}".format(ind))
+        return ans
